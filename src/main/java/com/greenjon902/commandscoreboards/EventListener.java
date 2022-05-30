a1b15c6913664284a5362167743ef4001d6633ce
greenjon902 <greenjon902@gmail.com>
2022-05-30 19:38:40 +0100

    added scoreboard controller

--- src/main/java/com/greenjon902/commandscoreboards/EventListener.java
+++ src/main/java/com/greenjon902/commandscoreboards/EventListener.java
@@ -1,6 +1,5 @@
 package com.greenjon902.commandscoreboards;
 
-import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,9 +8,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class EventListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().setScoreboard(
-                CommandScoreboards.getScoreboardHandler().makePlayerScoreboard(
-                    event.getPlayer().getUniqueId(),
-                    Component.text("Scoreboard")));
+        event.getPlayer().setScoreboard(CommandScoreboards.getScoreboardHandler().loadScoreboard(event.getPlayer().getUniqueId()));
     }
 }
--- src/main/java/com/greenjon902/commandscoreboards/ScoreboardHandler.java
+++ src/main/java/com/greenjon902/commandscoreboards/ScoreboardHandler.java
@@ -1,6 +1,7 @@
 package com.greenjon902.commandscoreboards;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
@@ -8,24 +9,56 @@ import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class ScoreboardHandler {
-    private final HashMap<UUID, Score[]> playerLines = new HashMap<>();
+    private final HashMap<UUID, ScoreboardController> playerScoreboardData = new HashMap<>();
 
-    public @NotNull Scoreboard makePlayerScoreboard(UUID playerUUID, Component displayName) {
-        Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-        Objective objective = scoreboard.registerNewObjective(playerUUID.toString(), "dummy", displayName);
+    public Scoreboard loadScoreboard(UUID playerId) {
+        if (playerScoreboardData.containsKey(playerId)) {
+            return playerScoreboardData.get(playerId).getScoreboard();
+        }
+        return null; //TODO: Make new scoreboards
+    }
+}
+
+class ScoreboardController {
+    private final Score[] scores = new Score[15];
+    private final String[] scoreNames = new String[15];
+    private final Scoreboard scoreboard;
+    private final Objective objective;
+
+    ScoreboardController(TextComponent displayName, String[] lines) {
+        scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        objective = scoreboard.registerNewObjective("data", "dummy", displayName);
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-        playerLines.put(playerUUID, new Score[15]);
-
-        for (int i=0; i<15; i++) {
-            Score line = objective.getScore(String.format("%1$"+i+"s", ""));
-            line.setScore(i);
-            playerLines.get(playerUUID)[i] = line;
+        if (lines.length == 15) {
+            throw new IllegalArgumentException("Scoreboard controller takes a line length of 15 only");
         }
+        for (int i=0; i<lines.length; i++) {
+
+            String line = lines[i];
+            while (Arrays.asList(scoreNames).contains(line)) {
+                line = line + " ";
+            }
+
+            Score score = objective.getScore(line);
+            score.setScore(i);
+            scores[i] = score;
+            scoreNames[i] = line;
+        }
+        System.out.println(Arrays.toString(scores));
+    }
+
+    public Scoreboard getScoreboard() {
         return scoreboard;
+    }
+
+    public Objective getObjective() {
+        return objective;
     }
 }
